da773db29550d5028cb5b280d178dc5cac21f484
Jean Marc BRUNO <technique@edps.fr>
2014-11-08 19:41:47 +0100

    adapter to ionicframework

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -10,7 +10,7 @@ public class BootLauncher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent serviceIntent = new Intent(context, uk.co.ilee.CordovaApp.class);
+        Intent serviceIntent = new Intent(context, com.ionicframework.starte.StarterApp.class);
         serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(serviceIntent);
     }
