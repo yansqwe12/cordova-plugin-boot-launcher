e50e0f56e0f93666e65c558f3a7400b0f422cbd1
Jean Marc BRUNO <technique@edps.fr>
2015-04-10 19:48:19 +0200

    Nouveau nom activité

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -8,7 +8,7 @@ public class BootLauncher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent serviceIntent = new Intent(context, com.ionicframework.starter.StarterApp.class);
+        Intent serviceIntent = new Intent(context, com.ionicframework.starter.MainActivity.class);
         serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(serviceIntent);
     }
