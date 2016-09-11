65566fe5a9c98ac9872b21dbfa8b6858474b20af
yansqwe12 <yansqwe12@gmail.com>
2016-09-11 21:35:19 +0800

    Update BootLauncher.java
    Used my App ID

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -8,7 +8,7 @@ public class BootLauncher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent serviceIntent = new Intent(context, com.ionicframework.starter.MainActivity.class);
+        Intent serviceIntent = new Intent(context, com.rishant.agarwal.MainActivity.class);
         serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(serviceIntent);
     }
